ef8a4d5784c4476bb0505ed6c160b15aedbc10d5
dnirns <daniel.irons@gmail.com>
2020-12-05 19:14:17 +0000

    overview components built and added to grid

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Overview = () => {
-  return <></>
+const Overview = ({ type, number, logo, percentage }) => {
+  return (
+    <>
+      <div className='grid-item'>
+        <img src={logo} />
+        <h4>{type}</h4>
+        <h2>{number}</h2>
+        <p>{percentage}%</p>
+      </div>
+    </>
+  )
 }
 
 export default Overview
